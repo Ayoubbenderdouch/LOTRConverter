84dbfc88684527cc2d21f07b7ca48496f6dc6a06
Ayoub Benderdouch <152822152+Ayoubbenderdouch@users.noreply.github.com>
2024-10-02 04:02:50 +0100

    Add files via upload

---
+++ LOTRConverter17/SelectCurrency.swift
@@ -0,0 +1,54 @@
+//
+//  SelectCurrency.swift
+//  LOTRConverter17
+//
+//  Created by Ayoub Benderdouch on 17/09/2024.
+//
+
+import SwiftUI
+
+struct SelectCurrency: View {
+    @Environment(\.dismiss) var dismiss
+//    @State var topCurrency = Currency.silverPenny
+//    @Binding var bottomCurrency: Currency = .goldPenny
+
+    @Binding var topCurrency: Currency
+    @Binding var bottomCurrency: Currency
+    
+    var body: some View {
+        ZStack {
+            Image(.parchment)
+                .resizable()
+                .ignoresSafeArea()
+                .background(.brown)
+            VStack {
+                Text("Select the currency you are starting with:")
+                    .fontWeight(.bold)
+                
+                //Currency icons
+                IconGrid(currency: $topCurrency)
+                
+                Text("Select the currency you would like to convert to:")
+                    .fontWeight(.bold)
+                
+                //Currency icons
+                IconGrid(currency: $bottomCurrency)
+                
+                Button("Done") {
+                    dismiss()
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.brown)
+                .font(.largeTitle)
+                .padding()
+                .foregroundStyle(.white)
+            }
+            .padding()
+            .multilineTextAlignment(.center)
+        }
+    }
+}
+
+#Preview {
+    SelectCurrency(topCurrency: .constant(.copperPenny), bottomCurrency: .constant(.silverPiece))
+}
